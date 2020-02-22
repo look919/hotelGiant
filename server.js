ab15bbf644819435e19b4f773566bbc1551bfad1
Wirkus Tomasz <wirkus919@gmail.com>
2020-02-22 11:28:27 +0100

    Heroku setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION - Shutting down...');
@@ -24,6 +25,14 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log('DB connection successful'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
