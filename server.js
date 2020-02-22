54ad14d1c10803d094efb0320730e9806823dba1
Wirkus Tomasz <wirkus919@gmail.com>
2020-02-22 16:17:09 +0100

    heroku issues v19

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
