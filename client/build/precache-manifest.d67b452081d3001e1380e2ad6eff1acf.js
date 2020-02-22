d9e35bd9013eea5b44c01307794c5b9af4985c00
Wirkus Tomasz <wirkus919@gmail.com>
2020-02-22 13:48:21 +0100

    heroku issues v6

---
+++ client/build/precache-manifest.d67b452081d3001e1380e2ad6eff1acf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0668d2cc5eee6321f3afd724b133271",
+    "url": "/static/media/family-room.a0668d2c.jpg"
+  },
+  {
+    "revision": "7c838bd941b8a8cdba5a",
+    "url": "/static/css/main.0eb21d23.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad2df82f7f4da38e0a6d98185c1eb492",
+    "url": "/static/media/room3.ad2df82f.jpg"
+  },
+  {
+    "revision": "ad161f0f05b566782958",
+    "url": "/static/js/2.4b6db1d6.chunk.js"
+  },
+  {
+    "revision": "4bce07396ed7f5f608de54f1f323d322",
+    "url": "/static/media/hotel-1.4bce0739.jpg"
+  },
+  {
+    "revision": "2e3491f521611c3c4de395a9d9e7a59a",
+    "url": "/static/media/hotel-2.2e3491f5.jpg"
+  },
+  {
+    "revision": "f15cea736ddbacb440b362cd157e8273",
+    "url": "/static/media/hotel-3.f15cea73.jpg"
+  },
+  {
+    "revision": "c696025b786e203d92706c20169db55d",
+    "url": "/static/media/gallery-gameroom.c696025b.jpg"
+  },
+  {
+    "revision": "25657672f7a790c419c06be7025bd519",
+    "url": "/static/media/gallery-gym.25657672.jpg"
+  },
+  {
+    "revision": "6e4964d2a527018cd0e1ab36b1071111",
+    "url": "/static/media/gallery-restaurant.6e4964d2.jpg"
+  },
+  {
+    "revision": "62cb9f33be22fe3415d72b4f8267a1ce",
+    "url": "/static/media/logo.62cb9f33.png"
+  },
+  {
+    "revision": "7c838bd941b8a8cdba5a",
+    "url": "/static/js/main.19ce6bdb.chunk.js"
+  },
+  {
+    "revision": "7e7fae5086eaa76717b0b4868c785431",
+    "url": "/static/media/gallery-hall.7e7fae50.jpg"
+  },
+  {
+    "revision": "137fec73b11daf4671828b78c085cca8",
+    "url": "/static/media/gallery-reception.137fec73.jpg"
+  },
+  {
+    "revision": "c99a52627f717601ebe86036625ab8d4",
+    "url": "/static/media/gallery-view.c99a5262.jpg"
+  },
+  {
+    "revision": "ad807683f398eebd9f3f716d0796cd37",
+    "url": "/static/media/room-double.ad807683.jpg"
+  },
+  {
+    "revision": "da1e92ccb2c33831ed35cf0a905464d5",
+    "url": "/static/media/room1.da1e92cc.jpg"
+  },
+  {
+    "revision": "3d218dba6c93e670d2a2c8e9ee12373a",
+    "url": "/static/media/apartament.3d218dba.jpg"
+  },
+  {
+    "revision": "a80b16e572599443e9764a3d1b13ce7e",
+    "url": "/static/media/form.a80b16e5.jpg"
+  },
+  {
+    "revision": "a7a9882c44cc5da64cffebf0789d5830",
+    "url": "/static/media/header.a7a9882c.jpg"
+  },
+  {
+    "revision": "e7bde8c3b27547fba5dff5e2f31530fa",
+    "url": "/static/media/room2.e7bde8c3.jpg"
+  },
+  {
+    "revision": "ad161f0f05b566782958",
+    "url": "/static/css/2.7db153ee.chunk.css"
+  },
+  {
+    "revision": "f6e48d09a2b63f8578b1754d7e50839c",
+    "url": "/index.html"
+  }
+];
